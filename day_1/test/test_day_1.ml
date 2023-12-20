6df5ac83572dee860c2434699ab470c9889f0d5c
amtoine <stevan.antoine@gmail.com>
2023-12-20 22:44:18 +0100

    load test file with a function

--- day_1/test/test_day_1.ml
+++ day_1/test/test_day_1.ml
@@ -3,13 +3,23 @@ open Alcotest
 let test f input expected () =
     check int "same" expected (f input)
 
-let silver = "1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet"
+let read_channel channel =
+    let buffer = Buffer.create 4096 in
+    let rec loop () =
+        let line = input_line channel in
+        Buffer.add_string buffer line;
+        Buffer.add_char buffer '\n';
+        loop ()
+    in
+        try loop () with
+            End_of_file -> Buffer.contents buffer
+
+let read_file filename =
+    let channel = open_in (Sys.getcwd () ^ "/../../../test/" ^ filename) in
+        read_channel channel
 
 let suite = [
-    "example", `Quick, test Day_1.silver silver 142
+    "example", `Quick, test Day_1.silver (read_file "silver.txt") 142
 ]
 
 let () =
