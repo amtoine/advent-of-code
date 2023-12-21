eab06147fe1b9da673f1733f94156a0ec73da47f
amtoine <stevan.antoine@gmail.com>
2023-12-21 15:58:00 +0100

    refactor file reading in library

--- solutions/2023/ocaml/day_1/bin/main.ml
+++ solutions/2023/ocaml/day_1/bin/main.ml
@@ -1,16 +1,1 @@
-let read_channel channel =
-    let buffer = Buffer.create 4096 in
-    let rec loop () =
-        let line = input_line channel in
-        Buffer.add_string buffer line;
-        Buffer.add_char buffer '\n';
-        loop ()
-    in
-        try loop () with
-            End_of_file -> Buffer.contents buffer
-
-let read_file filename =
-    let channel = open_in (Sys.getcwd () ^ "/" ^ filename) in
-        read_channel channel
-
-let () = print_int (Day_1.silver (read_file (Sys.argv.(1))))
+let () = print_int (Day_1.silver (Day_1.read_file (Sys.getcwd () ^ "/" ^ Sys.argv.(1))))
--- solutions/2023/ocaml/day_1/lib/day_1.ml
+++ solutions/2023/ocaml/day_1/lib/day_1.ml
@@ -1,3 +1,18 @@
+let read_channel channel =
+    let buffer = Buffer.create 4096 in
+    let rec loop () =
+        let line = input_line channel in
+        Buffer.add_string buffer line;
+        Buffer.add_char buffer '\n';
+        loop ()
+    in
+        try loop () with
+            End_of_file -> Buffer.contents buffer
+
+let read_file filename =
+    let channel = open_in filename in
+        read_channel channel
+
 let explode s = List.init (String.length s) (String.get s)
 
 let filter_digits = List.filter (
