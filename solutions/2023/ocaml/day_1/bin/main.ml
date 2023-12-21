b2bff8ce4da669062a0e71a0aaaa433616217918
amtoine <stevan.antoine@gmail.com>
2023-12-21 19:53:56 +0100

    get day step from command line

--- solutions/2023/ocaml/day_1/bin/main.ml
+++ solutions/2023/ocaml/day_1/bin/main.ml
@@ -1,1 +1,10 @@
-let () = print_int (Day_1.silver (Day_1__Fs.read_file (Sys.getcwd () ^ "/" ^ Sys.argv.(1))))
+let () =
+    let input_file = Sys.getcwd () ^ "/" ^ Sys.argv.(2) in
+    let input = Day_1__Fs.read_file input_file in
+    let res = match Sys.argv.(1) with
+        | "silver" -> Day_1.silver input
+        | "gold" -> Day_1.gold input
+        | x -> failwith (
+            "unknown command '" ^ x ^ "'" ^ " (expected 'silver' or 'gold')"
+        )
+    in print_int res
