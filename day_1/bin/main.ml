ce73e5267a6d761b18cbfdb79ccf25bb91582fe3
amtoine <stevan.antoine@gmail.com>
2023-12-21 10:41:38 +0100

    write binary

--- day_1/bin/main.ml
+++ day_1/bin/main.ml
@@ -1,1 +1,18 @@
-let () = print_endline "Hello, World!"
+let read_channel channel =
+    let buffer = Buffer.create 4096 in
+    let rec loop () =
+        let line = input_line channel in
+        Buffer.add_string buffer line;
+        Buffer.add_char buffer '\n';
+        loop ()
+    in
+        try loop () with
+            End_of_file -> Buffer.contents buffer
+
+let read_file filename =
+    let channel = open_in (Sys.getcwd () ^ "/" ^ filename) in
+        read_channel channel
+
+let () = print_endline (
+    string_of_int (Day_1.silver (read_file (Sys.argv.(1))))
+)
