bfeb361e874978132c9daf26a6bdd3217ef0fc20
amtoine <stevan.antoine@gmail.com>
2023-12-22 12:08:02 +0100

    fix expected of gold day 2

--- solutions/2023/ocaml/day_2/test/test_day_2.ml
+++ solutions/2023/ocaml/day_2/test/test_day_2.ml
@@ -7,7 +7,7 @@ let () =
     Alcotest.run "Day 2: Cube Conundrum" [
         "examples", [
             "silver", `Quick, test Day_2.silver (Day_2__Fs.read_file (data_dir ^ "silver.txt")) 8;
-            "gold", `Quick, test Day_2.gold (Day_2__Fs.read_file (data_dir ^ "gold.txt")) 8;
+            "gold", `Quick, test Day_2.gold (Day_2__Fs.read_file (data_dir ^ "gold.txt")) 2286;
             "amtoine-silver", `Quick, test Day_2.silver (Day_2__Fs.read_file (data_dir ^ "amtoine.txt")) 2164;
         ]
     ]
