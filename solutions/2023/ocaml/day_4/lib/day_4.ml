1d6e12c6e1f68f6ec7239419e7cfd873b5614c1c
amtoine <stevan.antoine@gmail.com>
2023-12-23 20:13:58 +0100

    write a function to parse a day 4 card

--- solutions/2023/ocaml/day_4/lib/day_4.ml
+++ solutions/2023/ocaml/day_4/lib/day_4.ml
@@ -1,3 +1,17 @@
+(** parse a list of numbers with format "x x x x x" *)
+let parse_numbers numbers =
+    String.split_on_char ' ' numbers
+        |> List.filter (fun x -> x <> "")
+        |> List.map int_of_string
+
+(** parse a card with format "...: x x x x | x x x x x" *)
+let parse_card card =
+    let raw = List.nth (String.split_on_char ':' card) 1 in
+    let winner_and_user = String.split_on_char '|' raw in (
+        parse_numbers (List.nth winner_and_user 0),
+        parse_numbers (List.nth winner_and_user 1)
+    )
+
 let silver input = String.length input
 
 let gold input = String.length input
