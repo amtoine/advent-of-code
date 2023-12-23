2dfabc99b0fc3c71e6460fc0f7dc8e54458c84af
amtoine <stevan.antoine@gmail.com>
2023-12-23 16:55:08 +0100

    init day 4

---
+++ solutions/2023/ocaml/day_4/bin/main.ml
@@ -0,0 +1,10 @@
+let () =
+    let input_file = Sys.getcwd () ^ "/../../_data/day_4/" ^ Sys.argv.(2) in
+    let input = Day_4__Fs.read_file input_file in
+    let res = match Sys.argv.(1) with
+        | "silver" -> Day_4.silver input
+        | "gold" -> Day_4.gold input
+        | x -> failwith (
+            "unknown command '" ^ x ^ "'" ^ " (expected 'silver' or 'gold')"
+        )
+    in print_int res
