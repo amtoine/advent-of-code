961f8791c9f4de557f3755cd82dca7b0d51c93c4
amtoine <stevan.antoine@gmail.com>
2023-12-23 20:26:01 +0100

    implement silver of day 4

--- solutions/2023/ocaml/day_4/lib/day_4.ml
+++ solutions/2023/ocaml/day_4/lib/day_4.ml
@@ -12,6 +12,22 @@ let parse_card card =
         parse_numbers (List.nth winner_and_user 1)
     )
 
-let silver input = String.length input
+(** compute x to the power of p
+
+    p should be positive.
+*)
+let rec pow x p = if p = 0 then 1 else x * (pow x (p - 1))
+
+let silver input =
+    let lines = String.trim input |> String.split_on_char '\n' in
+    List.map (fun l ->
+        let (w, u) = parse_card l in
+        let n = List.filter (fun x -> List.mem x w) u |> List.length in
+        if n >= 1 then
+            pow 2 (n - 1)
+        else
+            0
+    ) lines
+    |> List.fold_left (+) 0
 
 let gold input = String.length input
