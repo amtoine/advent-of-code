c3cdf315d96bdc041c294ed0f7057126b2f720f4
amtoine <stevan.antoine@gmail.com>
2023-12-23 20:30:39 +0100

    add my personal input for silver day 4

--- solutions/2023/ocaml/day_4/test/test_day_4.ml
+++ solutions/2023/ocaml/day_4/test/test_day_4.ml
@@ -7,6 +7,7 @@ let () =
     Alcotest.run "Day 4: Scratchcards" [
         "examples", [
             "silver", `Quick, test Day_4.silver (Day_4__Fs.read_file (data_dir ^ "silver.txt")) 13;
+            "amtoine-silver", `Quick, test Day_4.silver (Day_4__Fs.read_file (data_dir ^ "amtoine.txt")) 28750;
             (* "gold", `Quick, test Day_4.gold (Day_4__Fs.read_file (data_dir ^ "gold.txt")) xxx; *)
         ]
     ]
