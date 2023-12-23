1bb36b02f4b15b18d0155abc0e70534c848db2f2
amtoine <stevan.antoine@gmail.com>
2023-12-23 20:32:02 +0100

    don't hardcode the day tiltle in the OCaml template

--- templates/ocaml/test/test_day_x.ml
+++ templates/ocaml/test/test_day_x.ml
@@ -4,7 +4,7 @@ let test f input expected () =
 let data_dir = Sys.getcwd () ^ "/../../../../../_data/day_x/"
 
 let () =
-    Alcotest.run "Day 3: Gear Ratios" [
+    Alcotest.run xxx [
         "examples", [
             "silver", `Quick, test Day_x.silver (Day_x__Fs.read_file (data_dir ^ "silver.txt")) xxx;
             (* "gold", `Quick, test Day_x.gold (Day_x__Fs.read_file (data_dir ^ "gold.txt")) xxx; *)
