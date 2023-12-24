c2a33402897c7c17ee9ab15573490ca9b7361110
amtoine <stevan.antoine@gmail.com>
2023-12-24 19:13:33 +0100

    write silver of day 6

--- solutions/2023/ocaml/day_6/lib/day_6.ml
+++ solutions/2023/ocaml/day_6/lib/day_6.ml
@@ -1,2 +1,30 @@
-let silver input = String.length input
+let parse input =
+  let time_distance = String.split_on_char '\n' input in
+  let time =
+    List.nth (List.hd time_distance |> String.split_on_char ':') 1
+    |> String.split_on_char ' '
+    |> List.filter (fun x -> x <> "")
+    |> List.map int_of_string
+  in
+  let distance =
+    List.nth (List.nth time_distance 1 |> String.split_on_char ':') 1
+    |> String.split_on_char ' '
+    |> List.filter (fun x -> x <> "")
+    |> List.map int_of_string
+  in
+  List.combine time distance
+
+let silver input =
+  let races = parse input in
+  List.map
+    (fun (t, d) ->
+      let delta = float_of_int ((t * t) - (4 * d)) in
+      let s_delta = sqrt delta in
+      let t = float_of_int t in
+      let t_1 = int_of_float @@ floor (((t -. s_delta) /. 2.) +. 1.) in
+      let t_2 = int_of_float @@ ceil (((t +. s_delta) /. 2.) -. 1.) in
+      t_2 - t_1 + 1)
+    races
+  |> List.fold_left ( * ) 1
+
 let gold input = String.length input
