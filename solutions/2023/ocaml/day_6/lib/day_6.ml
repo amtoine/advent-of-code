94a525371b55c9a6927a245b2707488a9b550263
amtoine <stevan.antoine@gmail.com>
2023-12-24 19:20:40 +0100

    implement gold day 6

--- solutions/2023/ocaml/day_6/lib/day_6.ml
+++ solutions/2023/ocaml/day_6/lib/day_6.ml
@@ -27,4 +27,27 @@ let silver input =
     races
   |> List.fold_left ( * ) 1
 
-let gold input = String.length input
+let parse_gold input =
+  let time_distance = String.split_on_char '\n' input in
+  let time =
+    List.nth (List.hd time_distance |> String.split_on_char ':') 1
+    |> String.split_on_char ' '
+    |> List.filter (fun x -> x <> "")
+    |> List.fold_left ( ^ ) "" |> int_of_string
+  in
+  let distance =
+    List.nth (List.nth time_distance 1 |> String.split_on_char ':') 1
+    |> String.split_on_char ' '
+    |> List.filter (fun x -> x <> "")
+    |> List.fold_left ( ^ ) "" |> int_of_string
+  in
+  (time, distance)
+
+let gold input =
+  let t, d = parse_gold input in
+  let delta = float_of_int ((t * t) - (4 * d)) in
+  let s_delta = sqrt delta in
+  let t = float_of_int t in
+  let t_1 = int_of_float @@ floor (((t -. s_delta) /. 2.) +. 1.) in
+  let t_2 = int_of_float @@ ceil (((t +. s_delta) /. 2.) -. 1.) in
+  t_2 - t_1 + 1
