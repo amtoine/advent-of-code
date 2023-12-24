662e870bb57f079a1874fe80054431931d3bdbc6
amtoine <stevan.antoine@gmail.com>
2023-12-24 11:21:20 +0100

    add some documentation

--- solutions/2023/ocaml/day_5/lib/day_5.ml
+++ solutions/2023/ocaml/day_5/lib/day_5.ml
@@ -1,3 +1,16 @@
+(** split a list on some item
+
+    # Example
+    ```ocaml
+    split_list_at "" ["foo"; "bar"; ""; "baz"]
+    ```
+    will give `[["foo"; "bar"]; ["baz"]]`
+
+    ```ocaml
+    split_list_at "" ["foo"; "bar"; ""; "baz"; ""]
+    ```
+    will give `[["foo"; "bar"]; ["baz"]; []]`
+ *)
 let split_list_at v l =
   let rec aux v acc = function
     | [] -> [ acc ]
@@ -8,6 +21,46 @@ let split_list_at v l =
 
 type map = { dst : int; src : int; len : int }
 
+(** parse the input completely
+
+  # Example
+  from
+  ```
+  foos: 79 14 55 13
+
+  foo-to-bar map:
+  50 98 2
+  52 50 48
+
+  bar-to-baz map:
+  0 15 37
+  37 52 2
+
+  baz-to-yeah map:
+  49 53 8
+  0 11 42
+  ```
+  to
+  ```ocaml
+  (
+    [79; 14; 55; 13],
+    [
+      [
+        {dst = 50; src = 98; len = 2};
+        {dst = 52; src = 50; len = 48}
+      ];
+      [
+        {dst = 0; src = 15; len = 37};
+        {dst = 37; src = 52; len = 2}
+      ];
+      [
+        {dst = 49; src = 53; len = 8};
+        {dst = 0; src = 11; len = 42}
+      ]
+    ]
+  )
+  ```
+    *)
 let parse input =
   let cleaned =
     String.trim input |> String.split_on_char '\n'
