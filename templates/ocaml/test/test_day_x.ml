fee90ed31ff937c4dc7d45f9c058dd693701594d
amtoine <stevan.antoine@gmail.com>
2023-12-24 18:03:14 +0100

    update the template

--- templates/ocaml/test/test_day_x.ml
+++ templates/ocaml/test/test_day_x.ml
@@ -1,16 +1,22 @@
-let test f input expected () = Alcotest.(check int) "same" expected (f input)
 let data_dir = Sys.getcwd () ^ "/../../../../../_data/day_x/"
+
+let test name f input expected =
+  let input = Day_x__Fs.read_file (data_dir ^ input) in
+  let expected =
+    Day_x__Fs.read_file (data_dir ^ expected) |> String.trim |> int_of_string
+  in
+  (name, `Quick, fun () -> Alcotest.(check int) "same" expected (f input))
 
 let () =
   Alcotest.run xxx
     [
       ( "examples",
         [
-          ( "silver",
-            `Quick,
-            test Day_x.silver
-              (Day_x__Fs.read_file (data_dir ^ "silver.txt"))
-              xxx );
-          (* "gold", `Quick, test Day_x.gold (Day_x__Fs.read_file (data_dir ^ "gold.txt")) xxx; *)
+          test "silver" Day_x.silver "silver/input.txt" "silver/expected.txt";
+          (* test "gold" Day_x.gold "gold/input.txt" "gold/expected.txt"; *)
+          (* test "amtoine-silver" Day_x.silver "amtoine/input.txt" *)
+          (*   "amtoine/silver-expected.txt"; *)
+          (* test "amtoine-gold" Day_x.gold "amtoine/input.txt" *)
+          (*   "amtoine/gold-expected.txt"; *)
         ] );
     ]
