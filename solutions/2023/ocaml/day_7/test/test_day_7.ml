7a4871b29f088ea31a3a1f63f96972168e83ad99
amtoine <stevan.antoine@gmail.com>
2023-12-25 23:14:14 +0100

    add gold test for my personal input

--- solutions/2023/ocaml/day_7/test/test_day_7.ml
+++ solutions/2023/ocaml/day_7/test/test_day_7.ml
@@ -16,7 +16,7 @@ let () =
           test "gold" Day_7.gold "gold/input.txt" "gold/expected.txt";
           test "amtoine-silver" Day_7.silver "amtoine/input.txt"
             "amtoine/silver-expected.txt";
-          (* test "amtoine-gold" Day_7.gold "amtoine/input.txt" *)
-          (*   "amtoine/gold-expected.txt"; *)
+          test "amtoine-gold" Day_7.gold "amtoine/input.txt"
+            "amtoine/gold-expected.txt";
         ] );
     ]
