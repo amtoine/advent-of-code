49788d79c17d84db0e5fec911780d7e3e2c594fb
amtoine <stevan.antoine@gmail.com>
2023-12-26 13:11:31 +0100

    add documentation to `next`

--- solutions/2023/ocaml/day_9/lib/day_9.ml
+++ solutions/2023/ocaml/day_9/lib/day_9.ml
@@ -2,6 +2,36 @@ let parse input =
   String.split_on_char '\n' input
   |> List.map (fun s -> s |> String.split_on_char ' ' |> List.map int_of_string)
 
+(** [next s] is [n], where [n] is the element that _comes next_ in the sequence [s]
+
+    # Example
+
+    consider [s = [10; 13; 16; 21; 30; 45; 68]], if we compute the successive
+    sequences of consecutive differences between terms until we get to the
+    [[0; 0; ...; 0]] sequence, we get:
+    [
+    10  13  16  21  30  45 ??
+       3   3   5   9  15
+         0   2   4   6
+           2   2   2
+             0   0
+    ]
+
+    we can compute the unknown next element of [s], called [??], by adding a [0]
+    at the end of the last _0_ sequence and then going back up
+    [
+    10  13  16  21  30  45  68
+       3   3   5   9  15  23
+         0   2   4   6   8
+           2   2   2   2
+             0   0   0
+    ]
+
+    note that [68] is exactly the sum of all the last elements of each stage,
+    which is what [next s] computes.
+
+    see _Why don't they teach Newton's calculus of 'What comes next?'_ (https://youtu.be/4AuV93LOPcE)
+*)
 let next s =
   let rec aux acc s =
     if List.fold_left ( + ) 0 s = 0 then acc
