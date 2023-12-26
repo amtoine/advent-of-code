8c62cab4a5ace247463182178af45691a6b769c8
amtoine <stevan.antoine@gmail.com>
2023-12-26 17:08:32 +0100

    add gold tests

--- solutions/2023/ocaml/day_10/test/test_day_10.ml
+++ solutions/2023/ocaml/day_10/test/test_day_10.ml
@@ -14,7 +14,11 @@ let () =
         [
           test "silver-1" Day_10.silver "silver/1/input.txt" "silver/1/expected.txt";
           test "silver-2" Day_10.silver "silver/2/input.txt" "silver/2/expected.txt";
-          (* test "gold" Day_10.gold "gold/input.txt" "gold/expected.txt"; *)
+          test "gold-1" Day_10.gold "gold/1/input.txt" "gold/1/expected.txt";
+          test "gold-2" Day_10.gold "gold/2/input.txt" "gold/2/expected.txt";
+          test "gold-3" Day_10.gold "gold/3/input.txt" "gold/3/expected.txt";
+          test "gold-4" Day_10.gold "gold/4/input.txt" "gold/4/expected.txt";
+          test "gold-5" Day_10.gold "gold/5/input.txt" "gold/5/expected.txt";
           test "amtoine-silver" Day_10.silver "amtoine/input.txt"
             "amtoine/silver-expected.txt";
           (* test "amtoine-gold" Day_10.gold "amtoine/input.txt" *)
