0f2424ef0bc47797fa43812a92823160a365fa1d
amtoine <stevan.antoine@gmail.com>
2023-12-27 17:48:20 +0100

    add personal input and silver test day 11

--- solutions/2023/ocaml/day_11/test/test_day_11.ml
+++ solutions/2023/ocaml/day_11/test/test_day_11.ml
@@ -14,8 +14,8 @@ let () =
         [
           test "silver" Day_11.silver "silver/input.txt" "silver/expected.txt";
           (* test "gold" Day_11.gold "gold/input.txt" "gold/expected.txt"; *)
-          (* test "amtoine-silver" Day_11.silver "amtoine/input.txt" *)
-          (*   "amtoine/silver-expected.txt"; *)
+          test "amtoine-silver" Day_11.silver "amtoine/input.txt"
+            "amtoine/silver-expected.txt";
           (* test "amtoine-gold" Day_11.gold "amtoine/input.txt" *)
           (*   "amtoine/gold-expected.txt"; *)
         ] );
