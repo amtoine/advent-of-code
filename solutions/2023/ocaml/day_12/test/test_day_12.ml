3f591f0e3007e2777dbbfc787f0605b08eb2cc0c
amtoine <stevan.antoine@gmail.com>
2023-12-28 12:04:57 +0100

    mark amtoine-silver test of day 12 as slow

--- solutions/2023/ocaml/day_12/test/test_day_12.ml
+++ solutions/2023/ocaml/day_12/test/test_day_12.ml
@@ -1,20 +1,21 @@
 let data_dir = Sys.getcwd () ^ "/../../../../../../../data/2023/day_12/"
 
-let test name f input expected =
+let test name speed f input expected =
   let input = Day_12__Fs.read_file (data_dir ^ input) |> String.trim in
   let expected =
     Day_12__Fs.read_file (data_dir ^ expected) |> String.trim |> int_of_string
   in
-  (name, `Quick, fun () -> Alcotest.(check int) "same" expected (f input))
+  (name, speed, fun () -> Alcotest.(check int) "same" expected (f input))
 
 let () =
   Alcotest.run "Day 12: Hot Springs"
     [
       ( "examples",
         [
-          test "silver" Day_12.silver "silver/input.txt" "silver/expected.txt";
+          test "silver" `Quick Day_12.silver "silver/input.txt"
+            "silver/expected.txt";
           (* test "gold" Day_12.gold "gold/input.txt" "gold/expected.txt"; *)
-          test "amtoine-silver" Day_12.silver "amtoine/input.txt"
+          test "amtoine-silver" `Slow Day_12.silver "amtoine/input.txt"
             "amtoine/silver-expected.txt";
           (* test "amtoine-gold" Day_12.gold "amtoine/input.txt" *)
           (*   "amtoine/gold-expected.txt"; *)
